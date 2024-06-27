8c459ae6520236ce698ac98d5271cf250626e35e
Malcolm-Mac <malcolmprojects12@gmail.com>
2024-06-27 20:22:13 +0200

    refactoring status enumeration to use lombok

---
+++ src/main/java/com/malcolm/server/repository/ServerRepository.java
@@ -0,0 +1,9 @@
+package com.malcolm.server.repository;
+
+import com.malcolm.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server,  Long> {
+    Server findByIpAddress(String ipAddress);
+    Server findByName(String name);
+}
