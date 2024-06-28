e44635748ad4de2cbe3733cc60dde7e38a8bf99d
Malcolm-Mac <malcolmprojects12@gmail.com>
2024-06-28 08:06:04 +0200

    added: Server controller and implementation

--- src/main/java/com/malcolm/server/resource/ServerResource.java
+++ src/main/java/com/malcolm/server/resource/ServerResource.java
@@ -4,18 +4,20 @@ import com.malcolm.server.enumeration.Status;
 import com.malcolm.server.model.Response;
 import com.malcolm.server.model.Server;
 import com.malcolm.server.service.implementation.ServerServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/server")
@@ -48,6 +50,51 @@ public class ServerResource {
                         .statusCode(OK.value())
                         .build()
         );
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server)
+    {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+          Response.builder()
+                  .timeStamp(now())
+                  .data(Map.of("server", serverService.get(id)))
+                  .message("Server retrieved")
+                  .status(OK)
+                  .statusCode(OK.value())
+                  .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] getServerImage(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/image/" + fileName));
     }
 
 
